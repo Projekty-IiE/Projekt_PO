ee37b3db49e1951b6c28125a5e997173a196f7fb
Marcin <marcin.gorski1228@gmail.com>
2026-01-02 17:37:29 +0100

    Creating PortfolioView & testing

--- src/TradingSimulator.WPF/ViewModels/MainViewModel.cs
+++ src/TradingSimulator.WPF/ViewModels/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using TradingSimulator.Core.Interfaces;
+using System.Collections.ObjectModel;
+using TradingSimulator.Core.Models;
 
 namespace TradingSimulator.WPF.ViewModels
 {
@@ -25,10 +27,20 @@ namespace TradingSimulator.WPF.ViewModels
         [ObservableProperty]
         private decimal _totalValue;
 
+        public ObservableCollection<PortfolioItem> PortfolioItems { get; } = new();
+
         public MainViewModel(IMarketService marketService, IPortfolioService portfolioService)
         {
             _marketService = marketService;
             _portfolioService = portfolioService;
+
+            // test
+            try
+            {
+                _portfolioService.Buy("AAPL", 5);
+                _portfolioService.Buy("TSLA", 2);
+            }
+            catch { }
 
             RefreshData();
         }
@@ -40,8 +52,15 @@ namespace TradingSimulator.WPF.ViewModels
             int stocksCount = _marketService.Stocks.Count;
 
             StatusMessage = $"Data Refreshed. Cash: {Balance:C} " +
+                $"| Stocks' Value: {TotalValue-Balance:C} " +
                 $"| Portfolio's Value: {TotalValue:C} " +
                 $"| Market Stocks: {stocksCount}";
+
+            PortfolioItems.Clear();
+            foreach (var item in _portfolioService.Items)
+            {
+                PortfolioItems.Add(item);
+            }
         }
     }
 }
