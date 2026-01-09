f59f75dd2710dd4a2d8d8d71c45e08664e65e9b8
Marcin <marcin.gorski1228@gmail.com>
2026-01-09 17:11:38 +0100

    Creating TransactionMessage and MessageToColorConverter Displaying the messages independenty to StatusMessage

---
+++ src/TradingSimulator.WPF/Converters/MessageToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TradingSimulator.WPF.Converters
+{
+    internal class MessageToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string message && !string.IsNullOrEmpty(message))
+            {
+                if (message.StartsWith("Success", StringComparison.OrdinalIgnoreCase) ||
+                    message.StartsWith("SOLD", StringComparison.OrdinalIgnoreCase))
+                {
+                    return Brushes.Green;
+                }
+                else { return Brushes.Red; }
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
